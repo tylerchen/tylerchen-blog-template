11048a4758c657d387f3c5ddc4c3d3037ca3fd39
tylerchen <iffiff1@gmail.com>
2013-10-29 14:21:21 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,48 @@
-$(document).ready(function() {
-	var converter = new Showdown.converter();
+function load(url, callback) {  
+        var xhr;  
+          
+        if(typeof XMLHttpRequest !== 'undefined') xhr = new XMLHttpRequest();  
+        else {  
+            var versions = ["MSXML2.XmlHttp.5.0",   
+                            "MSXML2.XmlHttp.4.0",  
+                            "MSXML2.XmlHttp.3.0",   
+                            "MSXML2.XmlHttp.2.0",  
+                            "Microsoft.XmlHttp"]  
+  
+             for(var i = 0, len = versions.length; i < len; i++) {  
+                try {  
+                    xhr = new ActiveXObject(versions[i]);  
+                    break;  
+                }  
+                catch(e){}  
+             } // end for  
+        }  
+          
+        xhr.onreadystatechange = ensureReadiness;  
+          
+        function ensureReadiness() {  
+            if(xhr.readyState < 4) {  
+                return;  
+            }  
+              
+            if(xhr.status !== 200) {  
+                return;  
+            }  
+  
+            // all is well    
+            if(xhr.readyState === 4) {  
+                callback(xhr);  
+            }             
+        }  
+          
+        xhr.open('GET', url, true);  
+        xhr.send('');  
+    }
+function getURLParameter(name) {
+	return decodeURI((RegExp(name + '=' + '(.+?)(&|$)').exec(location.search) || [
+			, null ])[1]);
+}
+function markdownLoad(){
 	var url = getURLParameter("md");
 	{
 		if (url) {
@@ -12,10 +55,10 @@ $(document).ready(function() {
 	}
 	
 	try {
-		$.get(url, function(data) {
-			if (data) {
+		load(url, function(xhr) {
+			if (xhr && xhr.responseText) {
 				try {
-					$("#bodyColumn").html(converter.makeHtml(data));
+					document.getElementById("bodyColumn").innerHTML=markdown.toHTML(xhr.responseText);
 				} catch (err) {
 				}
 			}
@@ -24,15 +67,14 @@ $(document).ready(function() {
 	}
 	
 	try {
-		$("#leftColumn a").each(function(i) {
-			if (this.href && this.href.indexOf("md=" + url) > -1) {
-				$(this).parent().addClass("active");
+		var aArr=document.getElementsByTagName("a");
+		for(var i=0;i<aArr.length;i++){
+			var el=aArr[i];
+			if (el.href && el.href.indexOf("md=" + url) > -1) {
+				el.parentNode.className += ' active';
 			}
-		});
+		}
 	} catch (err) {
 	}
-});
-function getURLParameter(name) {
-	return decodeURI((RegExp(name + '=' + '(.+?)(&|$)').exec(location.search) || [
-			, null ])[1]);
 }
+window.onload = markdownLoad;
